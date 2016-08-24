558a0cc62208b67ad91b00ae49bce6508e633bae
Allison Hoke <allisonhoke@gmail.com>
2016-08-24 09:15:40 -0700

    adjusted method leap_year? to cleaner code.

--- 01-leap/leap.rb
+++ 01-leap/leap.rb
@@ -1,18 +1,20 @@
+# def leap_year?(year)
+#   if year % 4 == 0
+#     unless year % 400 == 0
+#       if year % 100 == 0
+#         return false
+#       end
+#     end
+#     return true
+#   end
+#   return false
+# end
+
+
 def leap_year?(year)
-  if year % 4 == 0
-    unless year % 400 == 0
-      if year % 100 == 0
-        return false
-      end
-    end
+  if year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
     return true
+  else
+    return false
   end
-  return false
 end
-
-
-  # unless year % 100 == 0
-  #   if year % 4 == 0
-  #     return true
-  #   end
-  # end
